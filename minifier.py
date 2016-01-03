e5db72ba8e052874d24e36087d970886085c3caf
Alexandre Baron <alexandre.baron@epitech.eu>
2016-01-03 13:02:22 +0100

    Start refactoring to fix spaced operators (e.g. "else"...)

--- minifier.py
+++ minifier.py
@@ -9,8 +9,12 @@ import sys
 import re
 import os  # SEEK_END etc.
 
+# Ops: ops that may be spaced out in the code but we can trim the whitespace
+# special ops are the same but for ops that may be mistaken for regex control characters so they are espaced
+# Spaced ops are ops that are defined in the C language to be delimited by spaces (keywords most of the time)
 OPS = ['+', '-', '*', '/', '+=', '-=', '*=', '/=', '=', '<', '>', '<=', '>=', ',', '(', ')', '{', '}', ';']
 SPECIAL_OPS = ['+', '*', '+=', '*=', '(', ')']
+SPACED_OPS = ['else']
 
 
 def remove_everything_between(subs1, subs2, line):
@@ -29,7 +33,7 @@ def remove_everything_past(subs, line):
 
 
 def remove_multiline_comments(lines):
-    start,end = '/*', '*/'
+    start, end = '/*', '*/'
     escaped_start, escaped_end = '/\*', '\*/'
     in_comment = False
     newlines = []
@@ -107,38 +111,53 @@ def get_args():
     return args
 
 
+def show_stats(source_file, minified_text):
+    # After "f.readlines", the file pointer is at file's end so tell() will return current file size.
+    orig_size = source_file.tell()
+    mini_size = len(minified_text)
+    delta = orig_size - mini_size
+    print(
+        "Original: {0} characters, Minified: {1} characters, {2} removed ({3:.1f}%)"
+        .format(orig_size, mini_size, delta, (float(delta) / float(orig_size)) * 100.0)
+    )
+
+
+def minify_source_file(args, filename):
+    newline = args.crlf
+    with open(filename) as f:
+        if args.names is True:
+            print("File {}:".format(source_file))
+        lines = f.readlines()
+        if args.keep_newline is False:
+            # Keep preprocessor lines (starting with #)
+            lines = map(lambda x: x.replace(newline, '') if not x.startswith('#') else x, lines)
+        lines = map(lambda x: x.replace('\t', ' '), lines)
+        # for each operator: remove space on each side of the op, on every line.
+        # Escape ops that could be regex control characters.
+        for op in OPS:
+            lines = map(minify_operator(op), lines)
+        # If it's a spaced op, do the contrary: ensure it is spaced out before and after.
+        for op in SPACED_OPS:
+            lines = map(space_operator(op), lines)
+        lines = trim(lines)  # erase leading and trailing whitespace
+        if args.keep_inline is False:
+            lines = remove_inline_comments(lines)
+        if args.keep_multiline is False:
+            lines = remove_multiline_comments(lines)
+        # Finally convert all remaining multispaces to single spaces
+        multi_spaces = re.compile(r'[  ]+ *')
+        lines = map(lambda string: multi_spaces.sub(' ', string), lines)
+        minified = ''.join(lines)
+        print(minified)
+        if args.stats is True:
+            show_stats(f, minified)
+
+
 def main():
     args = get_args()
-    newline = args.crlf
-    for source_file in args.files:
-        with open(source_file) as f:
-            if args.names is True:
-                print("File {}:".format(source_file))
-            lines = f.readlines()
-            if args.keep_newline is False:
-                # Keep preprocessor lines (starting with #)
-                lines = map(lambda x: x.replace(newline, '') if not x.startswith('#') else x, lines)
-            lines = map(lambda x: x.replace('\t', ' '), lines)
-            # Go through the ops list and remove on every line space on each side of the op
-            for op in OPS:
-                lines = map(minify_operator(op), lines)
-            lines = trim(lines)
-            if args.keep_inline is False:
-                lines = remove_inline_comments(lines)
-            if args.keep_multiline is False:
-                lines = remove_multiline_comments(lines)
-            multi_spaces = re.compile(r'[  ]+ *')
-            lines = map(lambda string: multi_spaces.sub(' ', string), lines)
-            minified = ''.join(lines)
-            print(minified)
-            if args.stats is True:
-                # After "f.readlines", the file pointer is at file's end so tell() will return current file size.
-                orig_size = f.tell()
-                mini_size = len(minified)
-                delta = orig_size - mini_size
-                print("Original: {0} characters, Minified: {1} characters, {2} removed ({3:.1f}%)"
-                      .format(orig_size, mini_size, delta, (float(delta) / float(orig_size)) * 100.0))
 
+    for filename in args.files:
+        minify_source_file(args, filename)
 
 if __name__ == "__main__":
     main()
